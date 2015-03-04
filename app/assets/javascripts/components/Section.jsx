b8df5fbf558f84abdc769e3b2aa43d289ca05d34
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-03-04 08:44:21 -0500

    add the click to the item back.

--- app/assets/javascripts/components/Section.jsx
+++ app/assets/javascripts/components/Section.jsx
@@ -2,10 +2,18 @@
 var React = require('react');
 
 var Section = React.createClass({
+  mixins: [CollectionUrlMixin],
+
   displayName: 'Section',
 
   propTypes: {
     section: React.PropTypes.object.isRequired,
+  },
+
+  getInitialState: function() {
+    return {
+      hover: false
+    };
   },
 
   style: function() {
@@ -17,17 +25,29 @@ var Section = React.createClass({
       marginLeft: '10px',
       marginRight: '10px',
       height: '100%',
+      cursor: (this.state.hover ? 'pointer' : 'auto'),
     }
   },
-
-
+  click: function () {
+    window.location.replace(this.sectionUrl(this.props.section));
+  },
+  onMouseEnter: function() {
+    return this.setState({
+      hover: true
+    });
+  },
+  onMouseLeave: function() {
+    return this.setState({
+      hover: false
+    });
+  },
   render: function() {
     var image;
     if (this.props.section.image) {
       image = (<SectionImage section={this.props.section} />);
     }
     return (
-      <div className="section" style={this.style()}>
+      <div className="section" style={this.style()} onClick={this.click} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} >
         {image}
         <SectionDescription section={this.props.section} />
       </div>
