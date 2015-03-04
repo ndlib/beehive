fea1c4856577557524f458b776c6c0393cb0d95a
Jaron Kennel <jaronkk@gmail.com>
2015-03-04 12:24:03 -0500

    Link sections using an a tag instead of onclick

--- app/assets/javascripts/components/Section.jsx
+++ app/assets/javascripts/components/Section.jsx
@@ -25,31 +25,40 @@ var Section = React.createClass({
       marginLeft: '10px',
       marginRight: '10px',
       height: '100%',
-      cursor: (this.state.hover ? 'pointer' : 'auto'),
-    }
+    };
   },
-  click: function () {
-    window.location.replace(this.sectionUrl(this.props.section));
+
+  linkStyle: function() {
+    return {
+      display: 'block',
+      height: '100%',
+      color: 'inherit',
+    };
   },
+
   onMouseEnter: function() {
     return this.setState({
       hover: true
     });
   },
+
   onMouseLeave: function() {
     return this.setState({
       hover: false
     });
   },
+
   render: function() {
     var image;
     if (this.props.section.image) {
       image = (<SectionImage section={this.props.section} />);
     }
     return (
-      <div className="section" style={this.style()} onClick={this.click} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} >
-        {image}
-        <SectionDescription section={this.props.section} />
+      <div className="section" style={this.style()} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} >
+        <a href={this.sectionUrl(this.props.section)} style={this.linkStyle()}>
+          {image}
+          <SectionDescription section={this.props.section} />
+        </a>
       </div>
     );
   }
