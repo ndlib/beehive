4aceb5cb8782f70447772fec00f7ba64d080479b
Dan Wolfe <dwolfe2@nd.edu>
2015-03-13 15:00:49 -0400

    fix for when prev or next section not there.

--- app/assets/javascripts/components/section/SectionShow.jsx
+++ app/assets/javascripts/components/section/SectionShow.jsx
@@ -8,16 +8,25 @@ var SectionShow = React.createClass({
   },
 
   render: function() {
-    var item ="";
+  console.log(this);
+    var item = "";
+    var prev = "";
+    var next = "";
     if (this.props.section) {
       if (this.props.section.item) {
         item = (<ItemShow item={this.props.section.item} />);
       }
+      if (this.props.section.previousSection) {
+        prev = (<PreviousSection section={this.props.section.previousSection} />);
+      }
+      if (this.props.section.nextSection) {
+        next = (<NextSection section={this.props.section.nextSection} />);
+      }
       return (
         <div>
           <h2>{this.props.section.title}</h2>
-          <PreviousSection section={this.props.section.previousSection} />
-          <NextSection section={this.props.section.nextSection} />
+          {prev}
+          {next}
           <div className="row">
             <div className="col-md-12">
               <div className="section-description">
