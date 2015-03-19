ca1573f73fac20af61e3e1860e5b30f64be7029a
Robert Fox <rfox2@nd.edu>
2015-03-19 17:39:48 -0400

    Ugh, correcting syntax again

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.3.5'
 set :application, 'beehive'
 set :repo_url, 'https://github.com/ndlib/beehive.git'
 
-set :logger_level, :info
+set :log_level, :info
 
 # Default branch is :master
 if fetch(:stage).to_s == 'production'
