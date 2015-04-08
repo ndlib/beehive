1559a39130cbbcf5f8eca56752003607f4a73cf3
Dan Wolfe <dwolfe2@nd.edu>
2015-04-08 14:54:32 -0400

    Add more link to sections where text overflows.

--- app/assets/javascripts/components/section/SectionDescription.jsx
+++ app/assets/javascripts/components/section/SectionDescription.jsx
@@ -5,7 +5,7 @@ var converter = new Showdown.converter()
 
 var SectionDescription = React.createClass({
   propTypes: {
-    section: React.PropTypes.object.isRequired
+    section: React.PropTypes.object.isRequired,
   },
 
   style: function() {
@@ -20,10 +20,47 @@ var SectionDescription = React.createClass({
     };
   },
 
+  overflowStyle: function() {
+    return {
+      position: 'absolute',
+      bottom: '3px',
+      zIndex: '2',
+      height: '60px',
+      lineHeight: '60px',
+      width: '392px',
+
+      textAlign: 'center',
+      marginLeft: '-14px',
+      background: 'black',
+      background: "-moz-linear-gradient(top, rgba(0,0,0,0.4) 0%, rgba(0,0,0,1) 55%, rgba(0,0,0,1) 100%)",
+      background: "-webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0.4)), color-stop(55%,rgba(0,0,0,1)), color-stop(100%,rgba(0,0,0,1)))",
+      background: "-webkit-linear-gradient(top, rgba(0,0,0,0.4) 0%,rgba(0,0,0,1) 55%,rgba(0,0,0,1) 100%)",
+      background: "-o-linear-gradient(top, rgba(0,0,0,0.4) 0%,rgba(0,0,0,1) 55%,rgba(0,0,0,1) 100%)",
+      background: "-ms-linear-gradient(top, rgba(0,0,0,0.4) 0%,rgba(0,0,0,1) 55%,rgba(0,0,0,1) 100%)",
+      background: "linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,1) 55%,rgba(0,0,0,1) 100%)",
+
+    };
+  },
+
+  componentDidMount: function() {
+    // we need to re-call the render function because the heights were not available
+    // the first time we rendered.
+    this.forceUpdate();
+  },
+
   render: function () {
     var rawMarkup = false;
     if (this.props.section.description) {
       rawMarkup = this.props.section.description.toString();
+    }
+
+    var sectionHeight = $('#section-' + this.props.section.id).height();
+    var contentHeight = $('#section-' + this.props.section.id + ' .item-description').height();
+    var overflow = "";
+    if(contentHeight !== null) {
+      if(contentHeight > sectionHeight) {
+        overflow = (<div style={this.overflowStyle()}>MORE</div>);
+      }
     }
 
     if (rawMarkup) {
@@ -31,6 +68,7 @@ var SectionDescription = React.createClass({
         <div className="section-container section-container-text" style={this.style()}>
           <h2>{this.props.section.title}</h2>
           <DescriptionTeaser description={rawMarkup} />
+          {overflow}
         </div>
       );
     } else {
