4ff9be447bc603debbf8626581e3a75cfb50b3fb
Dan Wolfe <dwolfe2@nd.edu>
2015-04-21 10:11:01 -0400

    Create a simple jest test for the Loading React component.

---
+++ app/assets/javascripts/components/__tests__/Loading-test.jsx
@@ -0,0 +1,21 @@
+// app/assets/javascripts/components/__tests__/Loading-test.jsx
+jest.dontMock('../other/Loading');
+
+
+describe('Loading', function() {
+  var React = require('react/addons');
+  var TestUtils = React.addons.TestUtils;
+  var Loading = require('../other/Loading');
+
+  var loading = TestUtils.renderIntoDocument(<Loading/>);
+
+  it('returns a div with the className loading', function() {
+    expect(loading.getDOMNode().nodeName).toBe('DIV');
+    expect(loading.getDOMNode().className).toBe('loading');
+  });
+
+  it('has an img tag with src=images/ajax-loader.gif', function() {
+    expect(loading.getDOMNode().firstChild.nodeName).toBe('IMG');
+    expect(loading.getDOMNode().innerHTML).toContain('src="/images/ajax-loader.gif"');
+  });
+});
