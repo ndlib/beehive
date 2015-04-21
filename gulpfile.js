625f76c1ee3b00cb5329f97cc8102d20cae87a4f
Dan Wolfe <dwolfe2@nd.edu>
2015-04-21 14:49:07 -0400

    ' -> "

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,36 @@
-var gulp = require('gulp');
-var jest = require('jest-cli');
-var watch = require('gulp-watch');
+var gulp = require("gulp");
+var jest = require("jest-cli");
+var watch = require("gulp-watch");
 
 var jestConfig = {
-    rootDir: './app/assets/javascripts/components',
-    scriptPreprocessor: '<rootDir>/__tests__/preprocessor.js',
+    rootDir: "./app/assets/javascripts/components",
+    scriptPreprocessor: "<rootDir>/__tests__/preprocessor.js",
       moduleFileExtensions: [
-        'js',
-        'jsx',
+        "js",
+        "jsx",
       ],
       unmockedModulePathPatterns: [
-        'react',
-        'jquery',
+        "react",
+        "jquery",
       ],
       testFileExtensions: [
-        'js',
-        'jsx',
+        """js",
+        "jsx",
       ],
       testPathIgnorePatterns: [
-        'preprocessor.js'
+        "preprocessor.js"
       ],
   };
 
-gulp.task('test', function(done) {
-    jest.runCLI({ config : jestConfig }, '.', function() {
+gulp.task("test", function(done) {
+    jest.runCLI({ config : jestConfig }, ".", function() {""""""""
         done();
     });
 });
 
-gulp.task('watch', function(done) {
-    gulp.watch([ jestConfig.rootDir + '/**/*.jsx' ], [ 'test' ]);
+gulp.task("watch", function(done) {
+    gulp.watch([ jestConfig.rootDir + "/**/*.jsx" ], [ "test" ]);
 });
 
-gulp.task('default', ['test', 'watch']);
+gulp.task("default", ["test", "watch"]);
 
