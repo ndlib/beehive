c116c752b2a7eafc3f723e931f2ceec72634134b
Dan Wolfe <dwolfe2@nd.edu>
2015-04-21 14:44:59 -0400

    quiet the hound

--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,33 @@ var jest = require('jest-cli');
 var watch = require('gulp-watch');
 
 var jestConfig = {
-    rootDir: "./app/assets/javascripts/components",
-    "scriptPreprocessor": "<rootDir>/__tests__/preprocessor.js",
-      "moduleFileExtensions": [
-        "js",
-        "jsx"
+    rootDir: './app/assets/javascripts/components',
+    scriptPreprocessor: '<rootDir>/__tests__/preprocessor.js',
+      moduleFileExtensions: [
+        'js',
+        'jsx',
       ],
-      "unmockedModulePathPatterns": [
-        "react",
-        "jquery"
+      unmockedModulePathPatterns: [
+        'react',
+        'jquery',
       ],
-      "testFileExtensions": [
-        "js",
-        "jsx"
+      testFileExtensions: [
+        'js',
+        'jsx',
       ],
-      "testPathIgnorePatterns": [
-        "preprocessor.js"
+      testPathIgnorePatterns: [
+        'preprocessor.js'
       ],
   };
 
 gulp.task('test', function(done) {
-    jest.runCLI({ config : jestConfig }, ".", function() {
+    jest.runCLI({ config : jestConfig }, '.', function() {
         done();
     });
 });
 
 gulp.task('watch', function(done) {
-    gulp.watch([ jestConfig.rootDir + "/**/*.jsx" ], [ 'test' ]);
+    gulp.watch([ jestConfig.rootDir + '/**/*.jsx' ], [ 'test' ]);
 });
 
 gulp.task('default', ['test', 'watch']);
