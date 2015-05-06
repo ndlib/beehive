0c1823b9bb43812484736e9ad35c212ba793e608
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-05-06 11:20:52 -0400

    change the modal to take the collection data.

--- app/assets/javascripts/components/modal/Info.jsx
+++ app/assets/javascripts/components/modal/Info.jsx
@@ -2,16 +2,25 @@
 var React = require('react');
 
 var Info = React.createClass({
+  propTypes: {
+    collection: React.PropTypes.object.isRequired,
+  },
+  customInfoText: function () {
+    if (this.props.collection.about) {
+      return (
+        <div className="about-collection">
+          <h2>About Collection</h2>
+          <div dangerouslySetInnerHTML={{__html: this.props.collection.about}} />
+        </div>
+      )
+    }
 
+    return ""
+  },
   content: function() {
     return (
       <div>
-        <div className="about-collection">
-          <h2>About Collection</h2>
-          <h3>Name of Curator</h3>
-          <p><em>Title or Position</em><br/>Hesburgh Libraries</p>
-          <p>p: 124-123-1234<br/>e: name@nd.edu</p>
-        </div>
+        {this.customInfoText()}
         <p>Exhibits is a service of the <a href="http://library.nd.edu">Hesburgh Libraries of Notre Dame</a>.</p>
         <p>For Technical Support call <a href="tel:5746316258">(574) 631-6258</a>  or email <a href="mailto:rfox2@nd.edu?subject=Support%20Request">rfox2@nd.edu</a>.</p>
       </div>
