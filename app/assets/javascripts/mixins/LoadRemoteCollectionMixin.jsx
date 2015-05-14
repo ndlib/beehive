e2636479c57b53fe73367a3dcac805e3c69dc87d
Dan Wolfe <dwolfe2@nd.edu>
2015-05-14 13:46:02 -0400

    Use jQuery for the ajax stuff. Jon said it was okay.

--- app/assets/javascripts/mixins/LoadRemoteCollectionMixin.jsx
+++ app/assets/javascripts/mixins/LoadRemoteCollectionMixin.jsx
@@ -1,26 +1,18 @@
 var LoadRemoteCollectionMixin = {
   loadRemoteCollection: function(url) {
     var url = url;
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-
-    request.onreadystatechange = function() {
-      if (request.readyState === 4){
-        if( request.status === 200) {
-          var result = JSON.parse(request.response);
-          if (this.isMounted()) {
-            this.setValues(result);
-          }
-        }
+    $.ajax({
+      context: this,
+      type: "GET",
+      url: url,
+      dataType: "json",
+      success: function(result) {
+        this.setValues(result);
+      },
+      error: function(request, status, thrownError) {
+        window.location = window.location.origin + "/404";
       }
-    }.bind(this);
-
-    request.onerror = function () {
-      window.location = window.location.origin + "/404";
-    }
-
-    request.send();
-
+    });
   },
 }
 
