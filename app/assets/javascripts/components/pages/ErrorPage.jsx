7de8497228200503306a2baa93db5afa39fe333e
Dan Wolfe <dwolfe2@nd.edu>
2015-05-14 12:46:50 -0400

    404 message.

--- app/assets/javascripts/components/pages/ErrorPage.jsx
+++ app/assets/javascripts/components/pages/ErrorPage.jsx
@@ -4,7 +4,6 @@ var ErrorPage = React.createClass({
   displayName: 'Error Page',
 
   render: function() {
-    var url = window.location.href;
     return (
     <div>
       <Layout>
@@ -14,7 +13,8 @@ var ErrorPage = React.createClass({
               <div className="col-lg-12 bee-page-content">
                 <div className="row">
                   <div className="col-sm-12">
-                    <p>There is nothing to see here at {url}.</p>
+                    <h1>Oops!</h1>
+                    <p>There doesn't appear to be anything here at the moment.</p>
                   </div>
                 </div>
               </div>
