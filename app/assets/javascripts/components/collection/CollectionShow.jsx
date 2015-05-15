fd26c411ab73cce43d94516a1cf4cda54c168fbd
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-05-15 10:58:28 -0400

    updates to the header image

--- app/assets/javascripts/components/collection/CollectionShow.jsx
+++ app/assets/javascripts/components/collection/CollectionShow.jsx
@@ -73,7 +73,7 @@ var CollectionShow = React.createClass({
           <div className="container collection-show">
             <div className="collection-text">
               <h1>{this.props.collection.name}</h1>
-              <Thumbnail image={this.props.collection.image} thumbnailType="medium" />
+              <Thumbnail image={this.props.collection.image} />
                   <a href={intro_url} className="start">Start <i className="mdi-navigation-chevron-right"></i></a>
               <div className="row">
                 <div className="collection-image col-md-12">
