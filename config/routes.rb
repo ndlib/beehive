004f27c8ea7293b439b0739166eeb41fb5957ef9
Dan Wolfe <dwolfe2@nd.edu>
2015-05-15 09:34:11 -0400

    Add support for routes missing the slug.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,27 @@
 Rails.application.routes.draw do
   root to: "collections#index"
 
+  get "/404" => "errors#not_found", via: :all
+
+  get "/:id", to: "collections#show"
   get "/:id/:slug", to: "collections#show"
   get "/:id/:slug/intro", to: "collections#intro"
 
   scope "/:collection_id/:collection_slug" do
     scope "/showcases" do
       get "/", to: "showcases#index"
+      get ":id/", to: "showcases#show"
+      get ":id/:slug", to: "showcases#show"
+    end
+    scope "/items" do
+      get "/", to: "items#index"
+    end
+  end
+
+  scope "/:collection_id" do
+    scope "/showcases" do
+      get "/", to: "showcases#index"
+      get ":id/", to: "showcases#show"
       get ":id/:slug", to: "showcases#show"
     end
     scope "/items" do
@@ -30,6 +45,6 @@ Rails.application.routes.draw do
 
   resources :styleguide
 
-  get "/404" => "errors#not_found", via: :all
+
 
 end
