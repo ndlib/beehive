69549ba24caf5de90f1e5c8ce1639e78cb5445a9
Dan Wolfe <dwolfe2@nd.edu>
2015-05-27 13:38:03 -0400

    Create thumbnail image for the "more" hover scroll down thingy.

--- app/assets/javascripts/components/showcase/StartShowcaseButton.jsx
+++ app/assets/javascripts/components/showcase/StartShowcaseButton.jsx
@@ -29,7 +29,7 @@ var StartShowcaseButton = React.createClass({
       if(this.props.collection.showcases.length > 0) {
         return (
             <div className="col-sm-4 well" style={this.style()}>
-              <h2 style={this.h2Style()}>Continue to</h2>
+              <h2 style={this.h2Style()}>Next Showcase</h2>
               <ShowcaseCard showcase={this.props.collection.showcases[0]} />
             </div>
         );
