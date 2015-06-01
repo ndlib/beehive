6f8ccbd1347a3b1d12106cbdbd25da062e0a872d
Jaron Kennel <jaronkk@gmail.com>
2015-06-01 08:55:32 -0400

    Fix short intro padding at small widths

--- app/assets/javascripts/components/collection/CollectionIntro.jsx
+++ app/assets/javascripts/components/collection/CollectionIntro.jsx
@@ -7,20 +7,9 @@ var CollectionIntro = React.createClass({
     id: React.PropTypes.string,
   },
 
-  style: function() {
-    if(this.props.height) {
-      return {
-        maxHeight: this.props.height + 'px',
-        overflow: 'hidden',
-      };
-    } else {
-      return {};
-    }
-  },
-
   render: function() {
     return (
-      <div style={this.style()} id={this.props.id}>
+      <div className="collection-short-intro-container" id={this.props.id}>
         <div className="collection-short-intro" dangerouslySetInnerHTML={{__html: this.props.collection.short_description}} />
       </div>
     )
