c9f9b2eff698fd7c8b39468b2d2560f3dd916aa9
Jaron Kennel <jaronkk@gmail.com>
2015-06-01 08:50:02 -0400

    Refactored GridList functions, made grid sizes into props

--- app/assets/javascripts/components/layout/GridList.jsx
+++ app/assets/javascripts/components/layout/GridList.jsx
@@ -2,10 +2,6 @@
 var React = require('react');
 
 var gridSize = 12;
-var grids = {
-  lg: 3,
-  sm: 2,
-};
 
 var GridList = React.createClass({
   propTypes: {
@@ -13,37 +9,55 @@ var GridList = React.createClass({
       React.PropTypes.array,
       React.PropTypes.object,
     ]).isRequired,
+    grids: React.PropTypes.object,
+  },
+
+  getDefaultProps: function () {
+    return {
+      grids: {
+        lg: 3,
+        sm: 2,
+      }
+    };
+  },
+
+  childGridNodes: function(node, index, nodeClass) {
+    var nodes = this.clearfixNodes(index);
+    nodes.push((
+      <div className={nodeClass} key={index}>
+        {node}
+      </div>
+    ));
+    return nodes;
+  },
+
+  clearfixNodes: function(index) {
+    var nodes = [];
+    if (index > 0) {
+      for (var prefix in this.props.grids) {
+        var columns = this.props.grids[prefix];
+        var clearClass = "clearfix visible-" + prefix + "-block";
+        if (index%columns == 0) {
+          nodes.push ((
+            <div className={clearClass} key={index + prefix + "clearfix"} />
+          ));
+        }
+      }
+    }
+    return nodes;
   },
 
   childrenGridNodes: function() {
     var index = 0;
     var childrenNodes = []
     var nodeClass = "";
-    for (var prefix in grids) {
-      var columns = grids[prefix];
+    for (var prefix in this.props.grids) {
+      var columns = this.props.grids[prefix];
       nodeClass += " col-" + prefix + "-" + (gridSize / columns);
     }
-    React.Children.forEach(this.props.children, function(node) {
-      var nodes = [];
-      if (index > 0) {
-        for (var prefix in grids) {
-          var columns = grids[prefix];
-          var clearClass = "clearfix visible-" + prefix + "-block";
-          if (index%columns == 0) {
-            nodes.push ((
-              <div className={clearClass} key={index + prefix + "clearfix"} />
-            ));
-          }
-        }
-      }
-      nodes.push((
-        <div className={nodeClass} key={index}>
-          {node}
-        </div>
-      ));
-      index += 1;
-      childrenNodes.push(nodes);
-    });
+    React.Children.forEach(this.props.children, function(node, index) {
+      childrenNodes.push(this.childGridNodes(node, index, nodeClass));
+    }.bind(this));
     return childrenNodes;
   },
 
