c21711cc4ed85d3ef6ddd4ce5f295348c8fb6b06
Dan Wolfe <dwolfe2@nd.edu>
2015-08-06 15:33:56 -0400

    starting style

--- app/assets/javascripts/mixins/DialogMixin.jsx
+++ app/assets/javascripts/mixins/DialogMixin.jsx
@@ -15,5 +15,68 @@ var DialogMixin = {
     ];
   },
 
+  componentWillMount: function() {
+    EventEmitter.on("ItemDialogWindow", this.showDisplayItemWindow);
+  },
+
+  getInitialState: function () {
+    return {
+      displayWindowActive: false,
+      currentItem: null,
+    };
+  },
+
+  showDisplayItemWindow: function(item) {
+    console.log("DISPLAY WINDOW");
+    this.setState({
+      displayWindowActive: true,
+      currentItem: item
+    });
+    this.refs.itemDialogWindow.show();
+  },
+
+  dismissMessage: function() {
+    this.hideDisplayItemWindow();
+  },
+
+  hideDisplayItemWindow: function() {
+    this.setState({
+      displayWindowActive: false,
+    });
+    this.refs.itemDialogWindow.dismiss();
+  },
+
+  dialogWindowStyle: function() {
+    return {
+      backgroundColor: 'black',
+      top: '86px',
+      zIndex: '1000'
+    };
+  },
+
+  displayItemWindow: function() {
+    return (
+      <Dialog
+        ref='itemDialogWindow'
+        modal={false}
+        openImmediately={false}
+        style={this.dialogWindowStyle()}
+        actions={this.okDismiss()}
+        contentStyle={{
+          width: '100%',
+          maxWidth: '100%'
+        }}
+        bodyStyle={{
+          color: 'white',
+          backgroundColor: 'black',
+          width: '100%',
+          maxWidth: '100%'
+        }}
+      >
+        <ItemShow item={this.state.currentItem} height={this.state.height} />
+      </Dialog>
+    );
+  },
+
 }
 module.exports = DialogMixin;
