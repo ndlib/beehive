abcdac78ef566389ae1c67bf0829fa752313c6df
Jaron Kennel <jaronkk@gmail.com>
2015-09-10 11:17:47 -0400

    Removed unused state variable in showcase, Move showcase inner content to a new component, limit updates via shouldComponentUpdate

---
+++ app/assets/javascripts/components/showcase/ShowcaseInnerContent.jsx
@@ -0,0 +1,37 @@
+//app/assets/javascripts/components/showcase/ShowcaseInnerContent.jsx
+var React = require("react");
+
+var ShowcaseInnerContent = React.createClass({
+  propTypes: {
+    showcase: React.PropTypes.object,
+    height: React.PropTypes.number.isRequired,
+  },
+
+  style: function() {
+    return {
+      position: "absolute",
+      height: this.props.height + "px",
+      top: 0,
+      left: 0,
+      overflowX: "visible",
+      overflowY: "visible",
+      paddingTop: "20px",
+    };
+  },
+
+  shouldComponentUpdate: function(nextProps, nextState) {
+    return JSON.stringify(this.props) !== JSON.stringify(nextProps);
+  },
+
+  render: function() {
+    return (
+      <div id="showcase-inner" className="showcase-inner" style={this.style()} >
+        <ShowcaseTitle height={this.props.height} showcase={this.props.showcase} />
+        <ShowcaseSections height={this.props.height} showcase={this.props.showcase} />
+      </div>
+    );
+  }
+});
+
+// each file will export exactly one component
+module.exports = ShowcaseInnerContent;
