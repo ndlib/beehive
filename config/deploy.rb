9664ac7c27af783844fe21615f499dee12d64021
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-09-11 10:09:37 -0400

    revert

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :log_level, :info
 
 # Default branch is set to a tag
 if fetch(:stage).to_s == "production"
-  set :branch, "v1.2"
+  set :branch, "v1.0"
 else
   if ENV["SCM_BRANCH"] && !(ENV["SCM_BRANCH"] == "")
     set :branch, ENV["SCM_BRANCH"]
