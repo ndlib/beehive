543d5791f42d12f892bc79947f1d77d14794465d
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-10-14 13:04:36 -0400

    move all the cares for showcases to their own directory.

--- app/assets/javascripts/components/showcase/ShowcaseSections.jsx
+++ app/assets/javascripts/components/showcase/ShowcaseSections.jsx
@@ -37,7 +37,7 @@ var ShowcaseSections = React.createClass({
   nextShowcase: function() {
     var nextShowcase;
     if(this.props.showcase.nextShowcase) {
-      nextShowcase = (<ShowcaseEnding height={this.props.height} showcase={this.props.showcase.nextShowcase} />);
+      nextShowcase = (<ShowcaseEndingCard height={this.props.height} showcase={this.props.showcase.nextShowcase} />);
     }
     return nextShowcase;
   },
