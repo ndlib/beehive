fa7fd51c59ec65cb366924899a3e7c1a248bd805
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-10-14 12:54:20 -0400

    update page footer.

--- app/assets/javascripts/components/layout/CollectionPageFooter.jsx
+++ app/assets/javascripts/components/layout/CollectionPageFooter.jsx
@@ -7,24 +7,16 @@ var CollectionPageFooter = React.createClass({
     collection: React.PropTypes.object.isRequired,
   },
 
-  closeOther: function(target) {
-    $(target).modal("hide");
-  },
-
   render: function () {
     return (
       <mui.Paper circle={false} rounded={false} zDepth={0} >
-        <footer id="footer" className="container">
-          <div className="row">
-            <div className="col-lg-12 pull-right">
-              <a href="http://library.nd.edu">
-                <img src="/images/ndmark300.png" className="hesburgh-logo" alt="University of Notre Dame Hesburgh Libraries" />
-              </a>
-              <a href="/">
-                <img src="/images/dec.logo-brokentoseeit.svg" className="dec-logo" alt="Digital Exhibits and Collections" />
-              </a>
-            </div>
-          </div>
+        <footer>
+          <a href="http://library.nd.edu">
+            Hesburgh Logo
+          </a>
+          <a href="/">
+            Dec Logo
+          </a>
         </footer>
       </mui.Paper>
     );
