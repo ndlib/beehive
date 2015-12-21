06316fb7e856e00b149a34a07908268f76e088bb
Jon Hartzler <jon.hartzler@gmail.com>
2015-12-21 11:54:11 -0500

    bump the version

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :log_level, :info
 
 # Default branch is set to a tag
 if fetch(:stage).to_s == "production"
-  set :branch, "v1.2.2"
+  set :branch, "v2.0.0"
 else
   if ENV["SCM_BRANCH"] && !(ENV["SCM_BRANCH"] == "")
     set :branch, ENV["SCM_BRANCH"]
