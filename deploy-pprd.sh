2661a33cbb4c9e0f0c346dfd6ed82bad567f83bc
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-07-12 15:41:50 -0400

    test current branch

--- deploy-pprd.sh
+++ deploy-pprd.sh
@@ -25,5 +25,6 @@ npm run build-pprd
 aws s3 sync ${DIR} s3://${BUCKET} --exclude '.*' --exclude '*.md' --delete --acl public-read
 
 echo ${BUCKET}.s3-website-us-east-1.amazonaws.com
+echo ${CURRENT_BRANCH}
 
 git checkout ${CURRENT_BRANCH}
