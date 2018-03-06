a3750f736e9390a6308cd70d8e2fd9052cde28f1
Dan Wolfe <dwolfe2@nd.edu>
2018-03-06 14:29:14 -0500

    Fix Google Analytics

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -13,59 +13,51 @@ import ErrorPage from './ErrorPage.jsx'
 import AboutPage from './AboutPage.jsx'
 import PagesPage from './PagesPage.jsx'
 import PrintableMetadata from './PrintableMetadata.jsx'
-
-const ga = require('react-ga')
-ga.initialize('UA-2118378-44')
-
-function logPageView () {
-  if (this.state.location.search === '') {
-    ga.pageview(this.state.location.pathname)
-  } else {
-    ga.pageview(this.state.location.pathname + '/' + this.state.location.search)
-  }
-}
+import Analytics from './Analytics.jsx'
 
 export default function () {
   return (
-    <BrowserRouter history={browserHistory} onUpdate={logPageView} >
-      <Switch>
-        <Route
-          exact path='/'
-          component={SiteIndexPage} />
-        <Route
-          exact path='/404'
-          component={ErrorPage} />
-        <Route
-          exact path='/metadata/:itemID'
-          component={PrintableMetadata} />
-        <Route
-          exact path='/:collectionID/:collectionSlug/intro'
-          component={CollectionIntroductionPage} />
-        <Route
-          exact path='/:collectionID/:collectionSlug/about'
-          component={AboutPage} />
-        <Route
-          exact path='/:collectionID/:collectionSlug/search'
-          component={SearchPage} />
-        <Route
-          exact path='/:collectionID/:collectionSlug/items/:itemID'
-          component={ItemPage} />
-        <Route
-          exact path='/:collectionID/:collectionSlug/showcases/:showcaseID/:showcaseSlug'
-          component={ShowcasePage} />
-        <Route
-          exact path='/:collectionID/:collectionSlug/showcases/:showcaseID/:showcaseSlug/sections/:sectionID'
-          component={SectionPage} />
-        <Route
-          exact path='/:collectionID/:collectionSlug/pages/:pageID/:pageSlug'
-          component={PagesPage} />
-        <Route
-          exact path='/:collectionID/:collectionSlug'
-          component={CollectionPage} />
-        <Route
-          exact path='/:customSlug'
-          component={CustomCollectionPage} />
-      </Switch>
+    <BrowserRouter history={browserHistory}>
+      <Analytics>
+        <Switch>
+          <Route
+            exact path='/'
+            component={SiteIndexPage} />
+          <Route
+            exact path='/404'
+            component={ErrorPage} />
+          <Route
+            exact path='/metadata/:itemID'
+            component={PrintableMetadata} />
+          <Route
+            exact path='/:collectionID/:collectionSlug/intro'
+            component={CollectionIntroductionPage} />
+          <Route
+            exact path='/:collectionID/:collectionSlug/about'
+            component={AboutPage} />
+          <Route
+            exact path='/:collectionID/:collectionSlug/search'
+            component={SearchPage} />
+          <Route
+            exact path='/:collectionID/:collectionSlug/items/:itemID'
+            component={ItemPage} />
+          <Route
+            exact path='/:collectionID/:collectionSlug/showcases/:showcaseID/:showcaseSlug'
+            component={ShowcasePage} />
+          <Route
+            exact path='/:collectionID/:collectionSlug/showcases/:showcaseID/:showcaseSlug/sections/:sectionID'
+            component={SectionPage} />
+          <Route
+            exact path='/:collectionID/:collectionSlug/pages/:pageID/:pageSlug'
+            component={PagesPage} />
+          <Route
+            exact path='/:collectionID/:collectionSlug'
+            component={CollectionPage} />
+          <Route
+            exact path='/:customSlug'
+            component={CustomCollectionPage} />
+        </Switch>
+      </Analytics>
     </BrowserRouter>
   )
 }
