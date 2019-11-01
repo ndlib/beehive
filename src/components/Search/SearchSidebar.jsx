d80904dfce372eb8e884c1b76a7b6670426e191b
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2019-11-01 09:21:17 -0400

    Fix facets not showing.

--- src/components/Search/SearchSidebar.jsx
+++ src/components/Search/SearchSidebar.jsx
@@ -4,20 +4,9 @@ import { Paper } from 'material-ui'
 const SearchFacets = require('./SearchFacets.jsx')
 
 const SearchSidebar = createReactClass({
-
-  getInitialState: function () {
-    return {
-      show: this.props.show,
-    }
-  },
-
-  toggleSidebar: function () {
-    this.setState({ show: !this.state.show })
-  },
-
   render: function () {
     return (
-      <Paper style={{ display: this.state.show ? 'block' : 'none', width: '25%', float: 'right' }} >
+      <Paper style={{ display: this.props.show ? 'block' : 'none', width: '25%', float: 'right' }} >
         <h3 style={{ paddingLeft:'16px' }}>Filter Results</h3>
         <hr />
         <SearchFacets />
