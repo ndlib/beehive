1d5df1a074d3f6fc5f7f32f3894478004647c493
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2020-03-11 14:09:30 -0400

    Replace google analytics with GTM.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -13,51 +13,48 @@ import ErrorPage from './ErrorPage.jsx'
 import AboutPage from './AboutPage.jsx'
 import PagesPage from './PagesPage.jsx'
 import PrintableMetadata from './PrintableMetadata.jsx'
-import Analytics from './Analytics.jsx'
 
 export default function () {
   return (
     <BrowserRouter history={browserHistory}>
-      <Analytics>
-        <Switch>
-          <Route
-            exact path='/'
-            component={SiteIndexPage} />
-          <Route
-            exact path='/404'
-            component={ErrorPage} />
-          <Route
-            exact path='/metadata/:itemID'
-            component={PrintableMetadata} />
-          <Route
-            exact path='/:collectionID/:collectionSlug/intro'
-            component={CollectionIntroductionPage} />
-          <Route
-            exact path='/:collectionID/:collectionSlug/about'
-            component={AboutPage} />
-          <Route
-            exact path='/:collectionID/:collectionSlug/search'
-            component={SearchPage} />
-          <Route
-            exact path='/:collectionID/:collectionSlug/items/:itemID'
-            component={ItemPage} />
-          <Route
-            exact path='/:collectionID/:collectionSlug/showcases/:showcaseID/:showcaseSlug'
-            component={ShowcasePage} />
-          <Route
-            exact path='/:collectionID/:collectionSlug/showcases/:showcaseID/:showcaseSlug/sections/:sectionID'
-            component={SectionPage} />
-          <Route
-            exact path='/:collectionID/:collectionSlug/pages/:pageID/:pageSlug'
-            component={PagesPage} />
-          <Route
-            exact path='/:collectionID/:collectionSlug'
-            component={CollectionPage} />
-          <Route
-            exact path='/:customSlug'
-            component={CustomCollectionPage} />
-        </Switch>
-      </Analytics>
+      <Switch>
+        <Route
+          exact path='/'
+          component={SiteIndexPage} />
+        <Route
+          exact path='/404'
+          component={ErrorPage} />
+        <Route
+          exact path='/metadata/:itemID'
+          component={PrintableMetadata} />
+        <Route
+          exact path='/:collectionID/:collectionSlug/intro'
+          component={CollectionIntroductionPage} />
+        <Route
+          exact path='/:collectionID/:collectionSlug/about'
+          component={AboutPage} />
+        <Route
+          exact path='/:collectionID/:collectionSlug/search'
+          component={SearchPage} />
+        <Route
+          exact path='/:collectionID/:collectionSlug/items/:itemID'
+          component={ItemPage} />
+        <Route
+          exact path='/:collectionID/:collectionSlug/showcases/:showcaseID/:showcaseSlug'
+          component={ShowcasePage} />
+        <Route
+          exact path='/:collectionID/:collectionSlug/showcases/:showcaseID/:showcaseSlug/sections/:sectionID'
+          component={SectionPage} />
+        <Route
+          exact path='/:collectionID/:collectionSlug/pages/:pageID/:pageSlug'
+          component={PagesPage} />
+        <Route
+          exact path='/:collectionID/:collectionSlug'
+          component={CollectionPage} />
+        <Route
+          exact path='/:customSlug'
+          component={CustomCollectionPage} />
+      </Switch>
     </BrowserRouter>
   )
 }
