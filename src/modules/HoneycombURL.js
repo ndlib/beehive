f07d7296d6a1942fe5503b826b636b5c0563de1b
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2020-05-27 10:44:35 -0400

    pprd -> prep

--- src/modules/HoneycombURL.js
+++ src/modules/HoneycombURL.js
@@ -3,7 +3,7 @@ export default function () {
   if (process.env.NODE_ENV === 'production') {
     return 'https://honeycomb.library.nd.edu'
   } else if (process.env.NODE_ENV === 'preproduction') {
-    return 'https://honeycombpprd-vm.library.nd.edu'
+    return 'https://honeycomb-prep.library.nd.edu'
   } else {
     return 'https://localhost:3017'
     // return 'https://honeycomb.library.nd.edu'
