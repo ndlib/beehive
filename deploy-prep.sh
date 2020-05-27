3ad0ac3b01d1b9d54dac5864d434993a1bbaa4ef
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2020-05-27 10:45:03 -0400

    Last one (I think)

--- deploy-prep.sh
+++ deploy-prep.sh
@@ -8,7 +8,7 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 echo -e "${RED}Building preproduction on branch ${BRANCH}${NC}"
-npm run build-pprd
+npm run build-prep
 echo ${REVISION} > build/REVISION
 
 aws s3 sync ./build s3://${BUCKET} --exclude '.*' --exclude '*.md' --delete --acl public-read
